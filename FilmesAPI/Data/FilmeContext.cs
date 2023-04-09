ceff362eb644f70de779e9c9877230df1e2284c2
andrevnl <andrevnl@hotmail.com>
2023-04-08 21:59:39 -0300

    Aula 4 - parte 1 - banco de dados

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -1,6 +1,14 @@
+using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace FilmesAPI.Data;
 
-public class FilmeContext
+public class FilmeContext : DbContext
 {
-    
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+    {
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
 }
